bb8ca4c71dfc2779ad39cc826c54dcab3cfde59b
Pedro Broso <84089773+pedrobbroso@users.noreply.github.com>
2022-06-07 20:32:49 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -9,13 +9,6 @@ CREATE TABLE cursos (
   PRIMARY KEY (Id)
 );
 
-CREATE TABLE estados (
-  Id int(11) NOT NULL AUTO_INCREMENT,
-  Estado varchar(60) DEFAULT NULL,
-  Uf varchar(60) DEFAULT NULL,
-  PRIMARY KEY (Id)
-);
-
 CREATE TABLE instrutor (
   Id int(11) NOT NULL AUTO_INCREMENT,
   Nome varchar(60) NOT NULL,
@@ -25,9 +18,7 @@ CREATE TABLE instrutor (
   Faculdade varchar(100) NOT NULL,
   cursosId int(11) NOT NULL,
   PRIMARY KEY (Id),
-  FOREIGN KEY (cursosId) REFERENCES cursos (id),
-  estadosId int(11) NOT NULL,
-  FOREIGN KEY (estadosId) REFERENCES estados (id)
+  FOREIGN KEY (cursosId) REFERENCES cursos (id)
 );
 
 CREATE TABLE turma (
@@ -37,37 +28,7 @@ CREATE TABLE turma (
   cursosId int(11) NOT NULL,
   instrutorId int(11) NOT NULL,
   PRIMARY KEY (Id),
-  FOREIGN KEY (cursosId) REFERENCES cursos (id),
-  FOREIGN KEY (instrutorId) REFERENCES instrutor (id)
+  FOREIGN KEY (cursosId) REFERENCES cursos (id)
 );
-
-INSERT INTO estados (id, Estado, Uf) VALUES
-(1, 'Acre', 'AC'),
-(2, 'Alagoas', 'AL'),
-(3, 'Amazonas', 'AM'),
-(4, 'Amapá', 'AP'),
-(5, 'Bahia', 'BA'),
-(6, 'Ceará', 'CE'),
-(7, 'Distrito Federal', 'DF'),
-(8, 'Espírito Santo', 'ES'),
-(9, 'Goiás', 'GO'),
-(10, 'Maranhão', 'MA'),
-(11, 'Minas Gerais', 'MG'),
-(12, 'Mato Grosso do Sul', 'MS'),
-(13, 'Mato Grosso', 'MT'),
-(14, 'Pará', 'PA'),
-(15, 'Paraíba', 'PB'),
-(16, 'Pernambuco', 'PE'),
-(17, 'Piauí', 'PI'),
-(18, 'Paraná', 'PR'),
-(19, 'Rio de Janeiro', 'RJ'),
-(20, 'Rio Grande do Norte', 'RN'),
-(21, 'Rondônia', 'RO'),
-(22, 'Roraima', 'RR'),
-(23, 'Rio Grande do Sul', 'RS'),
-(24, 'Santa Catarina', 'SC'),
-(25, 'Sergipe', 'SE'),
-(26, 'São Paulo', 'SP'),
-(27, 'Tocantins', 'TO');
 
 
