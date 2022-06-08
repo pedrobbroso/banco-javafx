2ca18202312f1e6c5531fe406eface92ff663dd9
Pedro Broso <84089773+pedrobbroso@users.noreply.github.com>
2022-06-08 15:27:04 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -9,6 +9,13 @@ CREATE TABLE cursos (
   PRIMARY KEY (Id)
 );
 
+CREATE TABLE estados (
+  Id int(11) NOT NULL AUTO_INCREMENT,
+  Estado varchar(60) DEFAULT NULL,
+  Uf varchar(60) DEFAULT NULL,
+  PRIMARY KEY (Id)
+);
+
 CREATE TABLE instrutor (
   Id int(11) NOT NULL AUTO_INCREMENT,
   Nome varchar(60) NOT NULL,
@@ -29,5 +36,34 @@ CREATE TABLE turma (
   PRIMARY KEY (Id),
   FOREIGN KEY (cursosId) REFERENCES cursos (id)
 );
+
+INSERT INTO estados (id, Estado, Uf) VALUES
+  (1, 'Acre', 'AC'),
+  (2, 'Alagoas', 'AL'),
+  (3, 'Amazonas', 'AM'),
+  (4, 'Amapá', 'AP'),
+  (5, 'Bahia', 'BA'),
+  (6, 'Ceará', 'CE'),
+  (7, 'Distrito Federal', 'DF'),
+  (8, 'Espírito Santo', 'ES'),
+  (9, 'Goiás', 'GO'),
+  (10, 'Maranhão', 'MA'),
+  (11, 'Minas Gerais', 'MG'),
+  (12, 'Mato Grosso do Sul', 'MS'),
+  (13, 'Mato Grosso', 'MT'),
+  (14, 'Pará', 'PA'),
+  (15, 'Paraíba', 'PB'),
+  (16, 'Pernambuco', 'PE'),
+  (17, 'Piauí', 'PI'),
+  (18, 'Paraná', 'PR'),
+  (19, 'Rio de Janeiro', 'RJ'),
+  (20, 'Rio Grande do Norte', 'RN'),
+  (21, 'Rondônia', 'RO'),
+  (22, 'Roraima', 'RR'),
+  (23, 'Rio Grande do Sul', 'RS'),
+  (24, 'Santa Catarina', 'SC'),
+  (25, 'Sergipe', 'SE'),
+  (26, 'São Paulo', 'SP'),
+  (27, 'Tocantins', 'TO');
 
 
